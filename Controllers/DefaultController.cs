0f7673a63ce28cb1c7bcc84f7f76e0951aa28878
İsa Kulaksız <isakulaksiz@outlook.com>
2020-12-24 16:15:07 +0300

    Add enpoint(/) and test

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quickstart.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            var responseObject = new { Status = "up" };
+            _logger.LogInformation($"Status pinged: {responseObject.Status}");
+            return responseObject;
+        }
+    }
+}
---
+++ Tests/DefaultControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace quickstart.Tests
+{
+    [Collection("Integration Tests")]
+    public class DefaultControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public DefaultControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetRoot_ReturnsSuccessAndStatusUp()
+        {
+            // arrange
+            var client = _factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync("/");
+
+            // assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(response.Content);
+            var responseObject = JsonSerializer.Deserialize<ResponseType>(await response.Content.ReadAsStringAsync(),
+                                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            Assert.Equal("Up", responseObject?.Status);
+        }
+
+        private class ResponseType
+        {
+            public String Status { get; set; }
+        }
+    }
+}
