633e20676b0d4707dc184874513838c2c44ea3c1
İsa Kulaksız <isakulaksiz@outlook.com>
2021-01-12 12:47:29 +0300

    feat: Add service orientation

---
+++ quickstart/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using quickstart.Models;
+
+namespace quickstart.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return new Student
+            {
+                Id = id,
+                Name = "isa kulaksız",
+                Class = new Class
+                {
+                    Id = 1,
+                    Name = "Master of Science",
+                    Teacher = new Teacher
+                    {
+                        Id = 1,
+                        Name = "Umit Atila",
+                        School = new School
+                        {
+                            Id = 1,
+                            Name = "Karabuk University",
+                            City = "Karabuk",
+                            State = "100.yıl"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
--- quickstart/Startup.cs
+++ quickstart/Startup.cs
@@ -23,7 +23,10 @@ namespace quickstart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options => {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
             services.AddHealthChecks();
         }
 
